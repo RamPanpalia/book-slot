f6d91839f6913eb32d5c2f0ff1749d0be1a3f70f
Ram <panpaliaram@gmail.com>
2022-10-09 15:23:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -14,44 +14,47 @@ import { CalendarPicker } from '@mui/x-date-pickers/CalendarPicker';
 
 function App() {
 
-  const [date, setDate] = React.useState(dayjs('2022-04-07'));
+  const [date, setDate] = React.useState(dayjs(new Date().toUTCString()));
+  console.log(new Date('2022-10-10T17:48:55Z').toUTCString())
   const [phone_id, setPhone_id] = useState('gAAAAABjPGG-aYwzpbW7FWrlHlKhWL76ZYqp5OjLd9h7mCa-BOrALaiZsv5359YZ0gVJ1gwpA4eflxPCc9sDYxvnEx4wnzGXgA==')
   const [start_time, setStart_time] = useState('2022-10-10T17:48:55Z')
   const [end_time, setEnd_time] = useState('2022-10-11T17:48:55Z')
   const [freeSlots, setFreeSlots] = useState(
-    [{
-      id: 1000,
-      name: 'Dr. Sunil Verma',
-      date: new Date(),
-      start: '3PM',
-      end: '3:30PM',
-    },
-    {
-      id: 1001,
-      name: 'Dr. Prabhakar Mishra',
-      date: new Date(),
-      start: '3:30PM',
-      end: '4PM',
-    },
-    {
-      id: 1002,
-      name: 'Dr. Ajay Raut',
-      date: new Date(),
-      start: '3PM',
-      end: '3:30PM',
-    },
-    {
-      id: 1003,
-      name: 'Dr. John Doe',
-      date: new Date(),
-      start: '4:30PM',
-      end: '5PM',
-    },
-    ]
+    // [{
+    //   id: 1000,
+    //   name: 'Dr. Sunil Verma',
+    //   date: new Date(),
+    //   start: '3PM',
+    //   end: '3:30PM',
+    // },
+    // {
+    //   id: 1001,
+    //   name: 'Dr. Prabhakar Mishra',
+    //   date: new Date(),
+    //   start: '3:30PM',
+    //   end: '4PM',
+    // },
+    // {
+    //   id: 1002,
+    //   name: 'Dr. Ajay Raut',
+    //   date: new Date(),
+    //   start: '3PM',
+    //   end: '3:30PM',
+    // },
+    // {
+    //   id: 1003,
+    //   name: 'Dr. John Doe',
+    //   date: new Date(),
+    //   start: '4:30PM',
+    //   end: '5PM',
+    // },
+    // ]
   );
 
-  function findSlots() {
-    fetch(`http://ec2-13-232-196-86.ap-south-1.compute.amazonaws.com/get_free_dates?phone_id=${phone_id}&start_time=${start_time}&end_time=${end_time}`)
+  async function findSlots() {
+    fetch(`http://ec2-13-232-196-86.ap-south-1.compute.amazonaws.com/get_free_dates?phone_id=${phone_id}&start_time=${start_time}&end_time=${end_time}`
+    ,{}
+    )
       .then((res) => { setFreeSlots(res) })
       .catch((err) => { console.log(err) })
   }
@@ -64,26 +67,20 @@ function App() {
         <div className='col col-1'>
           <div>Find Free Slots</div>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <CalendarPicker className='date-picker' date={date} onChange={(newDate) => { setDate(newDate) }} />
+            <CalendarPicker className='date-picker' date={date} onChange={(newDate) => { setDate(newDate);console.log(date);findSlots();}} />
           </LocalizationProvider>
-
-          <Button className='find-btn btn' appearance="primary" active onClick={findSlots}>
-            FIND
-          </Button>
         </div>
         <div className='col col-2'>
-          {freeSlots.map((ele) => {
+          {freeSlots?freeSlots.map((ele) => {
             return (
               <Slot
                 key={ele.id}
-                // name={ele.name}
                 date={ele.date.toString()}
                 from={ele.start}
                 till={ele.end}
               />)
-          })}
-          {/* <Slot/>
-        <Slot/> */}
+          }):<>Loading . . .</>}
+          
         </div>
       </div>
     </>
