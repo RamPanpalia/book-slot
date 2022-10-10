bbdc5ec7f8c5e5c47a07f6db250a9fa5e02ad207
Ram <panpaliaram@gmail.com>
2022-10-10 13:23:34 +0530

    booking_code_done

--- src/App.js
+++ src/App.js
@@ -1,42 +1,35 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import icon from './FOOZIE.webp'
 import Slot from './Slot'
 import axios from 'axios';
 import 'rsuite/dist/rsuite.min.css';
-import { Button } from 'rsuite';
 import dayjs from 'dayjs';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { CalendarPicker } from '@mui/x-date-pickers/CalendarPicker';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
-import {add} from 'date-fns'
+import { add } from 'date-fns'
 
 function App() {
 
   const [date, setDate] = React.useState(dayjs(new Date()));
   const [phone_id, setPhone_id] = useState('gAAAAABjPGG-aYwzpbW7FWrlHlKhWL76ZYqp5OjLd9h7mCa-BOrALaiZsv5359YZ0gVJ1gwpA4eflxPCc9sDYxvnEx4wnzGXgA==')
   const [start_time, setStart_time] = useState(new Date().toISOString())
-  const [end_time, setEnd_time] = useState(add(new Date(),{days: 1}).toISOString())
+  const [end_time, setEnd_time] = useState(add(new Date(), { days: 1 }).toISOString())
   const [freeSlots, setFreeSlots] = useState();
 
   async function findSlots() {
-    // fetch(`http://ec2-13-232-196-86.ap-south-1.compute.amazonaws.com/get_free_dates?phone_id=${phone_id}&start_time=${start_time}&end_time=${end_time}`
-    //   , {}
-    // )
-    //   .then((res) => { setFreeSlots(res) })
-    //   .catch((err) => { console.log(err) })
-
-    axios.get('http://ec2-13-232-196-86.ap-south-1.compute.amazonaws.com/get_free_dates',{
-      params:{
-        phone_id:phone_id,
-        start_time:start_time,
-        end_time:end_time,
+    axios.get('http://ec2-13-232-196-86.ap-south-1.compute.amazonaws.com/get_free_dates', {
+      params: {
+        phone_id: phone_id,
+        start_time: start_time,
+        end_time: end_time,
       }
     })
-    .then((res) => { setFreeSlots(res.data) })
-    .catch((err) => { console.log(err) })
+      .then((res) => { setFreeSlots(res.data) })
+      .catch((err) => { console.log(err) })
   }
 
   useEffect(() => {
@@ -48,30 +41,36 @@ function App() {
       <div className='head'>
         <img src={icon} alt="" />
       </div>
-      <div>Slots for {start_time} to {end_time}</div>
       <div className="App">
         <div className='col col-1'>
           <div>Find Free Slots</div>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <CalendarPicker 
-            className='date-picker' 
-            date={date} 
-            onChange={(newDate) => { 
-              setDate(newDate);
-              setStart_time(newDate.toISOString());
-              setEnd_time(add(new Date(newDate),{days: 1}).toISOString());
-              findSlots();
-            }} />
+            <CalendarPicker
+              className='date-picker'
+              date={date}
+              disablePast={true}
+              onChange={(newDate) => {
+                setDate(newDate);
+                setStart_time(newDate.toISOString());
+                setEnd_time(add(new Date(newDate), { days: 1 }).toISOString());
+                findSlots();
+              }} />
           </LocalizationProvider>
         </div>
         <div className='col col-2'>
-          {freeSlots!=null ? freeSlots.map((ele) => {
+          <div>
+            Available Slots from
+            {' '+ start_time.slice(0, 10)} {start_time.slice(11, 16) +' '}
+            to {end_time.slice(0, 10)} {end_time.slice(11, 16)}
+          </div>
+          {freeSlots != null ? freeSlots.map((ele) => {
             return (
               <Slot
                 key={ele.slot_id}
+                slot_id={ele.slot_id}
                 // date={new Date().toString()}
-                from={ele.start}
-                till={ele.end}
+                start_time={ele.start}
+                end_time={ele.end}
               />)
           }) : <div className='loading'>
             <Box sx={{ display: 'flex' }}>
--- src/Slot.js
+++ src/Slot.js
@@ -2,19 +2,68 @@ import React from 'react'
 import './App.css'
 import 'rsuite/dist/rsuite.min.css';
 import { Button } from 'rsuite';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Slot = (props) => {
+    //Slot already exists/booked
+    const [booking_status, setBookingStatus] = React.useState()
+    const notify = () =>{ 
+        toast(`${ booking_status }`, {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+        };
+    async function book_slot() {
+        axios.post(`http://ec2-13-232-196-86.ap-south-1.compute.amazonaws.com/book_slot?phone_id=gAAAAABjPGG-aYwzpbW7FWrlHlKhWL76ZYqp5OjLd9h7mCa-BOrALaiZsv5359YZ0gVJ1gwpA4eflxPCc9sDYxvnEx4wnzGXgA==&slot_id=${props.slot_id}`,
+            // {
+            //     params:{
+            //         phone_id:'gAAAAABjPGG-aYwzpbW7FWrlHlKhWL76ZYqp5OjLd9h7mCa-BOrALaiZsv5359YZ0gVJ1gwpA4eflxPCc9sDYxvnEx4wnzGXgA==',
+            //         slot_id:props.slot_id,
+            //     }
+            // }
+        )
+            .then((res => {
+                setBookingStatus(res.data.status);
+                notify();
+            }))
+            .catch((err) => {
+                setBookingStatus(err.response.data);
+                notify();
+            })
+        // console.log(props.slot_id)
+    }
+
     return (
         <div className='slot'>
             {/* <div className='name'>{props.name}</div> */}
             {/* <div className='Date'>{props.date.slice(0,24)}</div> */}
             <div className='time'>
-                <span className='from'>{props.from}</span>
+                <span className='from'>{props.start_time}</span>
                 -
-                <span className='till'>{props.till}</span>
+                <span className='till'>{props.end_time}</span>
                 <span className='Duration'>30 Min.</span>
             </div>
-            <Button className='book-btn btn' appearance="primary" active>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored"
+            />
+            <Button className='book-btn btn' appearance="primary" onClick={book_slot} active>
                 BOOK
             </Button>
         </div>
